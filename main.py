5224d8ae04a76055ca80881cb1a89d971f25f2ed
CharlieJ2005 <163343248+CharlieJ2005@users.noreply.github.com>
2025-07-24 20:20:24 +0200

    fix: lowercase help text

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ from terminal_solar_system.main import main
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--random", action="store_true", help="Randomise planets"
+        "--random", action="store_true", help="randomise planets"
     )
     parser.add_argument(
-        "--color", action="store_true", help="Enable color output"
+        "--color", action="store_true", help="enable color output"
     )
     parser.add_argument(
-        "--fps", type=int, default=30, help="Frames per second"
+        "--fps", type=int, default=30, help="frames per second"
     )
     parser.add_argument(
-        "--stars", type=int, default=100, help="Stars in the background"
+        "--stars", type=int, default=100, help="stars in the background"
     )
     parser.add_argument(
-        "--x-scale", type=float, default=2.2, help="Font height/width ratio"
+        "--x-scale", type=float, default=2.2, help="font height/width ratio"
     )
     args = parser.parse_args()
     main(args.fps, args.color, args.stars, args.x_scale, args.random)
